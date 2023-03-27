08681d31918eb4a2f5e079ab9d14d7640c41ed8d
Nshiv <kshiv1105@gmail.com>
2023-03-27 23:24:24 +0530

    Petstore project movement onto git

---
+++ src/test/java/endpoints/Routes.java
@@ -0,0 +1,21 @@
+package endpoints;
+
+
+
+public class Routes {
+
+    //base url
+    public static String base_url="https://petstore.swagger.io/v2";
+
+    //User Module
+    public static String post_url = base_url+"/user";
+    public static String get_url = base_url+"/user/{username}";
+    public static String update_url = base_url+"/user/{username}";
+    public static String delete_url = base_url+"/user/{username}";
+
+    //other modules
+
+
+
+
+}
---
+++ src/test/java/test/DDTests.java
@@ -0,0 +1,38 @@
+package test;
+
+import endpoints.UserEndPoints;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import payload.User;
+import utilities.DataProviders;
+
+
+public class DDTests {
+
+    @Test(priority = 0, dataProvider = "Data",dataProviderClass = DataProviders.class)
+    public void testPostUser(String userID,String username, String fname, String lname, String useremail, String pwd, String ph)
+    {
+        User userPayload = new User();
+        userPayload.setId(Integer.parseInt(userID));
+        userPayload.setUsername(username);
+        userPayload.setFirstName(fname);
+        userPayload.setLastName(lname);
+        userPayload.setEmail(useremail);
+        userPayload.setPassword(pwd);
+        userPayload.setPhone(ph);
+       // userPayload.setUserStatus(1);
+
+        Response response = UserEndPoints.createUser(userPayload);
+        response.then().log().all();
+
+        Assert.assertEquals(response.statusCode(),200);
+    }
+
+    @Test(priority = 1,dataProvider = "UserNames",dataProviderClass = DataProviders.class)
+    public void testDeleteUserByName(String username)
+    {
+      Response response = UserEndPoints.deleteUser(username) ;
+      Assert.assertEquals(response.getStatusCode(),200);
+    }
+}
